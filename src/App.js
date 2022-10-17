ab8d6b676e369ef0897bba7f24817e43193a859c
Hanif <hanif.aiubeee@gmail.com>
2022-10-17 12:58:12 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import CreateArea from "./components/CreateArea/CreateArea";
+import Header from "./components/Header/Header";
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <CreateArea />
+    </div>
+  );
+}
+
+export default App;
